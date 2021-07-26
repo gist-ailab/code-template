602ffa958fc7fcd9433940556b371a2b0fc09289
tjdhg456 <hogili89@gist.ac.kr>
2021-07-26 16:44:34 +0900

    Major Update: Logger, Resume for Detectron2

--- detectron2/train.py
+++ detectron2/train.py
@@ -38,10 +38,14 @@ from detectron2.evaluation import (
     SemSegEvaluator,
     verify_results,
 )
+from detectron2.data import DatasetMapper, build_detection_test_loader
 
 from detectron2.modeling import GeneralizedRCNNWithTTA
 import yaml
 import pathlib
+import module.hook
+import neptune.new as neptune
+from glob import glob
 
 def default_argument_parser(epilog=None):
     """
@@ -109,6 +113,10 @@ class Trainer(DefaultTrainer):
     are working on a new research project. In that case you can write your
     own training loop. You can use "tools/plain_train_net.py" as an example.
     """
+    def __init__(self, cfg, logger):
+        self.logger = logger
+        super(Trainer, self).__init__(cfg)
+
 
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
@@ -120,86 +128,91 @@ class Trainer(DefaultTrainer):
         """
         if output_folder is None:
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
-        evaluator_list = []
-        evaluator_type = MetadataCatalog.get(dataset_name).evaluator_type
-        if evaluator_type in ["sem_seg", "coco_panoptic_seg"]:
-            evaluator_list.append(
-                SemSegEvaluator(
-                    dataset_name,
-                    distributed=True,
-                    output_dir=output_folder,
-                )
-            )
-        if evaluator_type in ["coco", "coco_panoptic_seg"]:
-            evaluator_list.append(COCOEvaluator(dataset_name, output_dir=output_folder))
-        if evaluator_type == "coco_panoptic_seg":
-            evaluator_list.append(COCOPanopticEvaluator(dataset_name, output_folder))
-        if evaluator_type == "cityscapes_instance":
-            assert (
-                torch.cuda.device_count() >= comm.get_rank()
-            ), "CityscapesEvaluator currently do not work with multiple machines."
-            return CityscapesInstanceEvaluator(dataset_name)
-        if evaluator_type == "cityscapes_sem_seg":
-            assert (
-                torch.cuda.device_count() >= comm.get_rank()
-            ), "CityscapesEvaluator currently do not work with multiple machines."
-            return CityscapesSemSegEvaluator(dataset_name)
-        elif evaluator_type == "pascal_voc":
-            return PascalVOCDetectionEvaluator(dataset_name)
-        elif evaluator_type == "lvis":
-            return LVISEvaluator(dataset_name, output_dir=output_folder)
-        if len(evaluator_list) == 0:
-            raise NotImplementedError(
-                "no Evaluator for the dataset {} with the type {}".format(
-                    dataset_name, evaluator_type
-                )
-            )
-        elif len(evaluator_list) == 1:
-            return evaluator_list[0]
-        return DatasetEvaluators(evaluator_list)
 
-    @classmethod
-    def test_with_TTA(cls, cfg, model):
-        logger = logging.getLogger("detectron2.trainer")
-        # In the end of training, run an evaluation with TTA
-        # Only support some R-CNN models.
-        logger.info("Running inference with test-time augmentation ...")
-        model = GeneralizedRCNNWithTTA(cfg, model)
-        evaluators = [
-            cls.build_evaluator(
-                cfg, name, output_folder=os.path.join(cfg.OUTPUT_DIR, "inference_TTA")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder)
+
+    def build_hooks(self):
+        """
+        Build a list of default hooks, including timing, evaluation,
+        checkpointing, lr scheduling, precise BN, writing events.
+        Returns:
+            list[HookBase]:
+        """
+        cfg = self.cfg.clone()
+        cfg.defrost()
+        cfg.DATALOADER.NUM_WORKERS = 0  # save some memory and time for PreciseBN
+
+        ret = [
+            hooks.IterationTimer(),
+            hooks.LRScheduler(),
+            hooks.PreciseBN(
+                # Run at the same freq as (but before) evaluation.
+                cfg.TEST.EVAL_PERIOD,
+                self.model,
+                # Build a new data loader to not affect training
+                self.build_train_loader(cfg),
+                cfg.TEST.PRECISE_BN.NUM_ITER,
             )
-            for name in cfg.DATASETS.TEST
+            if cfg.TEST.PRECISE_BN.ENABLED and get_bn_modules(self.model)
+            else None,
         ]
-        res = cls.test(cfg, model, evaluators)
-        res = OrderedDict({k + "_TTA": v for k, v in res.items()})
-        return res
+
+        # Do PreciseBN before checkpointer, because it updates the model and need to
+        # be saved by checkpointer.
+        # This is not always the best: if checkpointing has a different frequency,
+        # some checkpoints may have more precise statistics than others.
+        if comm.is_main_process():
+            ret.append(hooks.PeriodicCheckpointer(self.checkpointer, cfg.SOLVER.CHECKPOINT_PERIOD))
+
+        def test_and_save_results():
+            self._last_eval_results = self.test(self.cfg, self.model)
+            return self._last_eval_results
+
+        # Do evaluation after checkpointer, because then if it fails,
+        # we can use the saved checkpoint to debug.
+        ret.append(module.hook.EvalHook(cfg.TEST.EVAL_PERIOD, test_and_save_results, logger=self.logger))
+
+        if comm.is_main_process():
+            # Here the default print/log frequency of each writer is used.
+            # run writers in the end, so that evaluation metrics are written
+            ret.append(hooks.PeriodicWriter(self.build_writers(), period=20))
+
+        # Custom Hook (Add-On) -> Loss Logging
+        ret.append(module.hook.LossEvalHook(
+            self.cfg.TEST.EVAL_PERIOD,
+            self.model,
+            build_detection_test_loader(
+                self.cfg,
+                self.cfg.DATASETS.TEST[0],
+                DatasetMapper(self.cfg,True)
+            ),
+            logger=self.logger))
+        return ret
 
 
-def setup(args):
+def setup(args, resume=False):
     """
     Create base_configs and perform basic setups.
     """
-    # load config from file and command-line arguments
-    with open(args.config_file, 'r') as f:
-        config_data = yaml.safe_load(f)
 
-    config_data['_BASE_'] = os.path.join(args.base_dir, config_data['_BASE_'])
-    config_data['MODEL']['WEIGHTS'] = os.path.join(args.base_dir, config_data['MODEL']['WEIGHTS'])
-    config_data['OUTPUT_DIR'] = os.path.join(args.base_dir, config_data['OUTPUT_DIR'])
+    if not resume:
+        # load config from file and command-line arguments
+        with open(args.config_file, 'r') as f:
+            config_data = yaml.safe_load(f)
 
-    with open(args.config_file.replace('.yaml', '_new.yaml'), "w") as f:
-        yaml.dump(config_data, f)
+        config_data['_BASE_'] = os.path.join(args.base_dir, config_data['_BASE_'])
+        config_data['MODEL']['WEIGHTS'] = os.path.join(args.base_dir, config_data['MODEL']['WEIGHTS'])
+        config_data['OUTPUT_DIR'] = os.path.join(args.base_dir, config_data['OUTPUT_DIR'])
 
-    args.config_file = args.config_file.replace('.yaml', '_new.yaml')
+        with open(args.config_file.replace('.yaml', '_new.yaml'), "w") as f:
+            yaml.dump(config_data, f)
+
+        args.config_file = args.config_file.replace('.yaml', '_new.yaml')
 
     # Configure File
     cfg = get_cfg()
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
-
-    # Test Options
-    cfg.TEST.EVAL_PERIOD = 500
 
     cfg.freeze()
     default_setup(cfg, args)
@@ -214,20 +227,44 @@ def register_data(args):
     register_coco_instances("KDN_val", {}, os.path.join(DATA_DIR, 'annotation', "det_val2017.json"),
                             os.path.join(DATA_DIR, 'images_enhancement', 'dark', '400'))
 
-def visualize_samples():
-    # visualize training data
-    my_dataset_train_metadata = MetadataCatalog.get("KDN_train")
-    dataset_dicts = DatasetCatalog.get("KDN_train")
-    import random
-    from detectron2.utils.visualizer import Visualizer
-    for d in random.sample(dataset_dicts, 3):
-        img = cv2.imread(d["file_name"])
-        visualizer = Visualizer(img[:, :, ::-1], metadata=my_dataset_train_metadata, scale=0.5)
-        vis = visualizer.draw_dataset_dict(d)
-        cv2_imshow(vis.get_image()[:, :, ::-1])
-
 def main(args):
-    cfg = setup(args)
+    args.config_file = '/home/sung/src/code-template/detectron2/checkpoint_enhancement/config.yaml'
+    args.resume = True
+    args.log = True
+    args.neptune_id = None
+
+    cfg = setup(args, resume=args.resume)
+
+    # Logger
+    token = 'eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI5MTQ3MjY2Yy03YmM4LTRkOGYtOWYxYy0zOTk3MWI0ZDY3M2MifQ=='
+
+    if args.log:
+        mode = 'async'
+    else:
+        mode = 'debug'
+
+    if args.resume:
+        neptune_id = os.path.basename(glob(os.path.join(cfg.OUTPUT_DIR, 'ID_*'))[0])
+        neptune_id = neptune_id.split('_')[-1]
+        run = neptune.init('sunghoshin/test', api_token=token,
+                           capture_stdout=False,
+                           capture_stderr=False,
+                           capture_hardware_metrics=False,
+                           run=neptune_id,
+                           mode=mode
+                           )
+
+    else:
+        run = neptune.init('sunghoshin/test', api_token=token,
+                           capture_stdout=False,
+                           capture_stderr=False,
+                           capture_hardware_metrics=False,
+                           mode=mode
+                           )
+
+        neptune_id = str(run.__dict__['_short_id'])
+        with open(os.path.join(cfg.OUTPUT_DIR,  'ID_%s' %neptune_id), 'w') as f:
+            pass
 
     # Register New Dataset
     register_data(args)
@@ -238,25 +275,20 @@ def main(args):
             cfg.MODEL.WEIGHTS, resume=args.resume
         )
         res = Trainer.test(cfg, model)
-        if cfg.TEST.AUG.ENABLED:
-            res.update(Trainer.test_with_TTA(cfg, model))
+
         if comm.is_main_process():
             verify_results(cfg, res)
         return res
+
 
     """
     If you'd like to do anything fancier than the standard training logic,
     consider writing your own training loop (see plain_train_net.py) or
     subclassing the trainer.
     """
-    trainer = Trainer(cfg)
+    trainer = Trainer(cfg, logger=run)
     trainer.resume_or_load(resume=args.resume)
-    if cfg.TEST.AUG.ENABLED:
-        trainer.register_hooks(
-            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
-        )
     return trainer.train()
-
 
 if __name__ == "__main__":
     # Load Default Arguments
@@ -275,5 +307,3 @@ if __name__ == "__main__":
         dist_url=args.dist_url,
         args=(args,),
     )
-
-
